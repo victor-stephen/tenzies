eb646abac81dc5d71db838d6cc38e7c8f887304e
victor-stephen <victorstephen047@gmail.com>
2025-06-04 22:23:33 +0100

    improve accessibility

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,7 @@
 export default function Die({ dieObj, holdDice }) {
   // console.log(dieObj)
   const styles = {
-    background: dieObj.isHeld ? "#59E391":"#fff",
+    background: dieObj.isHeld ? "#59E391" : "#fff",
     boxShadow: "0px 2px 2px 0px rgba(0, 0, 0, 0.15)",
     borderRadius: "5px",
     textAlign: "center",
@@ -10,13 +10,17 @@ export default function Die({ dieObj, holdDice }) {
     alignItems: "center",
     lineHeight: "60px",
     border: "none",
-  }
+  };
 
   return (
     <button
       onClick={holdDice}
       className="die"
       style={styles}
+      aria-pressed={dieObj.isHeld}
+      aria-label={`Die with value ${dieObj.value}, ${
+        dieObj.isHeld ? "held" : "not held"
+      } `}
     >
       {dieObj.value}
     </button>
